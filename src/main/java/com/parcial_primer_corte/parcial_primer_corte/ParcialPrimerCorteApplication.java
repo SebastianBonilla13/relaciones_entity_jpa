6139b5c42051db3e802ac3c959bbd9c1ce1a4a2d
dmesa <dm31284@gmail.com>
2024-09-30 10:56:16 -0500

    guardar asignatura

--- src/main/java/com/parcial_primer_corte/parcial_primer_corte/ParcialPrimerCorteApplication.java
+++ src/main/java/com/parcial_primer_corte/parcial_primer_corte/ParcialPrimerCorteApplication.java
@@ -1,13 +1,46 @@
 package com.parcial_primer_corte.parcial_primer_corte;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.parcial_primer_corte.parcial_primer_corte.data.models.Course;
+import com.parcial_primer_corte.parcial_primer_corte.data.models.Location;
+import com.parcial_primer_corte.parcial_primer_corte.data.models.Subject;
+import com.parcial_primer_corte.parcial_primer_corte.data.repositories.LocationRepository;
+import com.parcial_primer_corte.parcial_primer_corte.data.repositories.SubjectRepository;
+
+import jakarta.transaction.Transactional;
+
 @SpringBootApplication
-public class ParcialPrimerCorteApplication {
+@Transactional
+public class ParcialPrimerCorteApplication implements CommandLineRunner {
+
+	@Autowired
+	private SubjectRepository subjectRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParcialPrimerCorteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		cerateSubject();
+	}
+
+	private void cerateSubject() {
+		List<Course> cursos = new ArrayList<Course>();
+		Subject asignatura = new Subject();
+		asignatura.setCode("1234");
+		asignatura.setName("Las guacas");
+		asignatura.setCourses(cursos);
+
+		Subject newSubject = subjectRepo.save(asignatura);
+		System.out.println("SE HA CREADO LA NUEVA ASIGNATURA: " + newSubject);
 	}
 
 }
--- src/main/java/com/parcial_primer_corte/parcial_primer_corte/data/repositories/TimeSlot.java
+++
@@ -1,7 +0,0 @@
-package com.parcial_primer_corte.parcial_primer_corte.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TimeSlot extends JpaRepository<TimeSlot, Integer> {
-
-}
