9325fc90458bbd549e946aad6cdeef3beeaaf567
dmesa <dm31284@gmail.com>
2024-09-30 10:37:40 -0500

    ajuste persona controller

--- src/main/java/com/parcial_primer_corte/parcial_primer_corte/Controllers/PersonController.java
+++ src/main/java/com/parcial_primer_corte/parcial_primer_corte/Controllers/PersonController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@RequestMapping("/api/v1/parcial")
+@RequestMapping("/api/v1/parcial/persona")
 public class PersonController {
 
     private final IPersonService service;
